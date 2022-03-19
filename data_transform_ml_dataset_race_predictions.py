31158d7a284d8246843877b34b1439a48fe9e3c0
Pedro Rodrigues <p3dror@gmail.com>
2022-03-19 17:11:36 +0100

    test

--- data_transform_ml_dataset_race_predictions.py
+++ data_transform_ml_dataset_race_predictions.py
@@ -28,40 +28,45 @@ cat_teams = [t for t in list(set(list(fact_practices["Team"].unique()) + list(fa
 
 data_ml = []
 for i, date in enumerate(valid_race_dates):
+    if i > 0:
+        fact_races_loading = fact_races[fact_races["date"] <= date]
+        fact_practices_loading = fact_practices[fact_practices["date"] <= date]
+        fact_qualifying_loading = fact_qualifying[fact_qualifying["date"] <= date]
 
-    fact_races_loading = fact_races[fact_races["date"] <= date]
-    fact_practices_loading = fact_practices[fact_practices["date"] <= date]
-    fact_qualifying_loading = fact_qualifying[fact_qualifying["date"] <= date]
+        print("Transforming: " + str(date))
+        #Predicting goal
+        fact_races_now = fact_races_loading[fact_races_loading["date"] == date]
+        season = list(fact_races_now["season"].unique())[0]
+        track = list(fact_races_now["track"].unique())[0]
+        df_loading = fact_races_now[["Rank", "PointsPts", "Driver", "Team", "date", "track", "season"]]
+        df_loading.rename(inplace=True, columns={"Rank": "Future_Rank", "PointsPts": "Future_Points"})
 
-    print("Transforming: " + str(date))
-    #Predicting goal
-    fact_races_now = fact_races_loading[fact_races_loading["date"] == date]
-    season = list(fact_races_now["season"].unique())[0]
-    track = list(fact_races_now["track"].unique())[0]
-    df_loading = fact_races_now[["Rank", "PointsPts", "Driver", "Team", "date", "track", "season"]]
-    df_loading.rename(inplace=True, columns={"Rank": "Future_Rank", "PointsPts": "Future_Points"})
+        #Last Practice
+        fact_practices_now = fact_practices_loading[(fact_practices_loading["track"] == track) & (fact_practices_loading["season"] == season)]
+        fact_practices_now.drop(columns=["date", "results_type", "Fastest Lap"], inplace=True)
+        list_pivot_columns = ["Rank", "Laps", "practice_number", "time_diff_s", "time_diff_ms"]
+        list_nonpivot_columns = [c for c in list(fact_practices_now.columns) if c not in list_pivot_columns]
+        for pn in list(fact_practices_now["practice_number"]):
+            fact_practices_now[["p"+str(pn)+"_"+c for c in list_pivot_columns]] = fact_practices_now.loc[fact_practices_now["practice_number"]==pn, list_pivot_columns]
+        fact_practices_now = fact_practices_now[[c for c in list(fact_practices_now.columns) if c not in list_pivot_columns ]]
+        fact_practices_now = fact_practices_now.groupby(list_nonpivot_columns)[list(fact_practices_now.columns)].agg('max') #pd.DataFrame(fact_practices_now.max(axis=0)).T
+        fact_practices_now.reset_index(drop=True, inplace=True)
+        df_loading = pd.merge(df_loading, fact_practices_now, how="left", on=["Driver", "Team", "track", "season"])
 
-    #Last Practice
-    fact_practices_now = fact_practices_loading[(fact_practices_loading["track"] == track) & (fact_practices_loading["season"] == season)]
-    fact_practices_now.drop(columns=["date", "results_type", "Fastest Lap"], inplace=True)
-    list_pivot_columns = ["Rank", "Laps", "practice_number", "time_diff_s", "time_diff_ms"]
-    list_nonpivot_columns = [c for c in list(fact_practices_now.columns) if c not in list_pivot_columns]
-    for pn in list(fact_practices_now["practice_number"]):
-        fact_practices_now[["p"+str(pn)+"_"+c for c in list_pivot_columns]] = fact_practices_now.loc[fact_practices_now["practice_number"]==pn, list_pivot_columns]
-    fact_practices_now = fact_practices_now[[c for c in list(fact_practices_now.columns) if c not in list_pivot_columns ]]
-    fact_practices_now = fact_practices_now.groupby(list_nonpivot_columns)[list(fact_practices_now.columns)].agg('max') #pd.DataFrame(fact_practices_now.max(axis=0)).T
-    fact_practices_now.reset_index(drop=True, inplace=True)
-    df_loading = pd.merge(df_loading, fact_practices_now, how="left", on=["Driver", "Team", "track", "season"])
+        #Last Qualifying
+        fact_qualifying_now = fact_qualifying_loading[(fact_qualifying_loading["track"] == track) & (fact_qualifying_loading["season"] == season)]
+        fact_qualifying_now.drop(columns=["date", "results_type", "Qualifying 1Q1", "Qualifying 2Q2", "Qualifying 3Q3", "Time", "BestTime"], inplace=True)
+        df_loading = pd.merge(df_loading, fact_qualifying_now, how="left", on=["Driver", "Team", "track", "season"])
 
-    #Last Qualifying
-    fact_qualifying_now = fact_qualifying_loading[(fact_qualifying_loading["track"] == track) & (fact_qualifying_loading["season"] == season)]
-    fact_qualifying_now.drop(columns=["date", "results_type", "Qualifying 1Q1", "Qualifying 2Q2", "Qualifying 3Q3", "Time", "BestTime"], inplace=True)
-    df_loading = pd.merge(df_loading, fact_qualifying_now, how="left", on=["Driver", "Team", "track", "season"])
+        #Last Race
+        fact_races_now = fact_races_loading[fact_races_loading["date"] < date]
+        fact_races_now = fact_races_now[fact_races_now["date"] == fact_races_now["date"].max()]
+        fact_races_now.drop(columns=["date", "results_type", "Qualifying 1Q1", "Qualifying 2Q2", "Qualifying 3Q3", "Time", "BestTime"], inplace=True)
 
-    assert df_loading.isnull().values[df_loading.isnull().values == True].size / df_loading.size < 0.10
+        assert df_loading.isnull().values[df_loading.isnull().values == True].size / df_loading.size < 0.10
 
-    data_ml.append(df_loading)
-    # print("db")
+        data_ml.append(df_loading)
+        # print("db")
 
 df_ml = pd.concat(data_ml)
 df_ml["season"] = pd.Categorical(df_ml["season"], categories=cat_seasons)
--- ml_train_test.py
+++ ml_train_test.py
@@ -9,13 +9,14 @@ import sklearn
 from sklearn.datasets import make_classification
 from lightgbm import LGBMClassifier
 from flaml import AutoML
-from sklearn.metrics import accuracy_score, roc_auc_score, f1_score
+from sklearn.metrics import accuracy_score, roc_auc_score, f1_score, precision_score
+pd.set_option("display.max_columns", 10)
 
 ################# PARAMETERS ###################
 prediction_feature = "Future_Winner"
+top_n = 10
 time_cross_validation_test_date_start = pd.to_datetime('2021-01-01')
 ################################################
-
 
 df_ml = pd.read_feather(r"C:\F1-Forecast\DWH/ml_dataset_race_predictions")
 
@@ -26,56 +27,69 @@ df_ml["Future_Podium"] = np.where(df_ml["Future_Rank"] <= 3, 1, 0)
 #Model configuration
 automl_clf = AutoML()
 automl_settings = {
-    "time_budget": 30,  # in seconds
+    "time_budget": 5,  # in seconds
     "metric": 'roc_auc',
-    "task": 'classification'
+    "task": 'classification',
+    "verbose": 0
     }
 
 #time series cross validation
 race_dates = list(df_ml["date"].unique())
 list_y_pred = []
+list_y_pred_binary = []
 list_y_test = []
-for race_date in race_dates:
+list_accurate_ranks = []
+race_cnt = 1
+for i, race_date in enumerate(race_dates):
     if race_date >= time_cross_validation_test_date_start:
         #Train Test Split
         df_ml_here = df_ml[df_ml["date"] <= race_date]
-        df_ml_here = df_ml_here[[c for c in list(df_ml_here.columns) if "Future" not in c or c == prediction_feature]]
+        # df_ml_here = df_ml_here[[c for c in list(df_ml_here.columns) if "Future" not in c or c == prediction_feature]]
 
         df_train = df_ml_here[df_ml_here["date"] < race_date]
         y_train = df_train[prediction_feature]
-        x_train = df_train[[c for c in list(df_train.columns) if c not in [prediction_feature, "date"]]]
+        x_train = df_train[[c for c in list(df_train.columns) if "Future" not in c and c not in [prediction_feature, "date"]]]
 
         df_test = df_ml_here[df_ml_here["date"] == race_date]
         y_test = df_test[prediction_feature]
-        x_test = df_test[[c for c in list(df_test.columns) if c not in [prediction_feature, "date"]]]
+        x_test = df_test[[c for c in list(df_test.columns) if  "Future" not in c and c not in [prediction_feature, "date"]]]
 
         # Train Model
         automl_clf.fit(x_train, y_train, **automl_settings, estimator_list=["lgbm"])
         y_pred = list(automl_clf.predict_proba(x_test)[:,1])
-        list_y_pred.append(y_pred)
-        list_y_test.append(y_test)
+        list_y_pred.extend(y_pred)
+        list_y_test.extend(list(y_test))
 
-        results_here = x_test[["Driver","Team","track","season"]]
+        results_here = x_test.copy()[["Driver","Team","track","season"]]
         results_here["preds"] = y_pred
         results_here["win_prob"] = [int(100*i/np.sum(y_pred)) for i in y_pred]
-        results_here.sort_values(by="win_prob", ascending=False, inplace=True)
+        results_here["Future_Rank"] = df_test["Future_Rank"]
+        results_here.sort_values(by="preds", ascending=False, inplace=True)
         results_here.reset_index(drop=True, inplace=True)
+        results_here.index = np.arange(1, len(results_here) + 1)
+        results_here["RankAcc"] = np.where(results_here["Future_Rank"] <= results_here.index, 1, 0)
+        print("\n########### PREDICTING " + list(x_test["track"])[0] + " - " + str(list(x_test["season"])[0]) + " ###########" )
+        print("### Predictions Top "+str(top_n)+" - ["+prediction_feature+"] - ###")
+        print(results_here[["Driver", "Team", "preds", "win_prob", "Future_Rank", "RankAcc"]].head(top_n))
 
-        print("############ Predictions Top 5 - ["+prediction_feature+"] - ############")
-        print(results_here.head(5))
-        print("#########################"+"#"*len(prediction_feature)+"################")
+        y_pred_binary = [1 if np.argmax(y_pred) == i else 0 for i in range(len(y_pred))]
+        list_y_pred_binary.extend(y_pred_binary)
 
-        y_pred_binary = [1 if np.argmax(list_y_pred) == i else 0 for i in range(len(list_y_pred))]
+        list_accurate_ranks.extend(list(results_here["RankAcc"]))
+        rank_acc = round(np.sum(list_accurate_ranks) / len(list_accurate_ranks), 2)
+        print("Rank-Accuracy["+str(race_cnt)+"] = " + str(round(rank_acc,2)))
 
-        acc = accuracy_score(y_test,y_pred)
+        acc = accuracy_score(list_y_test, list_y_pred_binary)
+        print(prediction_feature+"-Accuracy["+str(race_cnt)+"] = " + str(round(acc,2)))
+        pr = precision_score(list_y_test, list_y_pred_binary)
+        print(prediction_feature+"-Precision["+str(race_cnt)+"] = " + str(round(pr,2)))
+        auc = roc_auc_score(list_y_test, list_y_pred)
+        print(prediction_feature+"-ROC-AUC["+str(race_cnt)+"] = " + str(round(auc,2)))
+        f1 = f1_score(list_y_test, list_y_pred_binary)
+        print(prediction_feature+"-F1-Score["+str(race_cnt)+"] = " + str(round(f1,2)))
+        race_cnt += 1
 
-
-        print(race_date)
-
-
-
-
-
-
+        print("#########################"+"#"*len(prediction_feature)+"######################\n")
+        print("")
 
 print("END")
